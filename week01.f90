2016c198d09d13e3d7499266b1e0659f452f5ab6
Sanjay Paraboo <paraboosanjay@gmail.com>
2020-09-19 19:43:59 -0400

    Week 01 commit

---
+++ week01.f90
@@ -0,0 +1,11 @@
+program week01
+
+real r, pi, a
+r = 47;
+pi = 3.14159
+a = pi * (r**2)
+
+print *, 'Hello, my name is Sanjay and my student number is 215014947.'
+print *, 'The area of a 47m-radius circle is: ', a
+
+end program week01
